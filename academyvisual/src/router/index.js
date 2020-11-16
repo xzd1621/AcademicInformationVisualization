d7707425ca81fa15ed05c14e6294698a235866b0
xuzhida <xuzhida@xuzhidadeMacBook-Pro.local>
2020-11-16 20:25:15 +0800

    增加url

--- academyvisual/src/router/index.js
+++ academyvisual/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import wordCloud from "../components/academyWordCloud";
+import test from "../components/test";
+import halibote from "../components/halibote";
+import mnist from "../components/mnist";
 
 Vue.use(Router)
 
@@ -10,6 +14,27 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+
+    {
+      path: '/wordCloud',
+      name: 'wordCloud',
+      component: wordCloud
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: test
+    },
+    {
+      path: '/halibote',
+      name: 'halibote',
+      component: halibote
+    },
+    {
+      path: '/mnist',
+      name: 'mnist',
+      component: mnist
+    },
   ]
 })
